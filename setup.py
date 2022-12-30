0819c9d9b699a68bc61787d8ce6d4a4bf44ea054
BSENSOR\antons <anton.sauchanka@brolis-sensor.com>
2022-12-30 17:37:09 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='algoport',
+    version='0.1.0',
+    description='Algorithmic portfolio management framework',
+    url='https://github.com/astekas/algoport.git',
+    author='Anton Sauchanka',
+    author_email='anton.sauchanka@gmail.com',
+    license='BSD 2-clause',
+    packages=['algoport'],
+    install_requires=['matplotlib==3.6.1',
+                     'mealpy==2.5.1',
+                      'numpy==1.23.3',
+                      'pandas==1.4.4',
+                      'plotly==5.9.0',
+                      'pymoo==0.6.0',
+                      'qpsolvers[open_source_solvers]',
+                      'rpy2==3.5.5',
+                      'scipy==1.9.3',
+                      'statsmodels==0.13.2',
+                      'Cython==0.29.32',
+                      'pyarrow==10.0.1'
+                      ],
+)
